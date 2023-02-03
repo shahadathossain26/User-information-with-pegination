04449e83a98ed426c3b6aeb0c0ba92df1851809f
shahadathossain26 <shiimran172372@gmail.com>
2023-02-03 16:24:22 +0600

    pegination added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import User from '../User/User';
+import ReactPaginate from 'react-paginate';
 
 const Home = () => {
     const [users, setUsers] = useState([]);
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const userPerPage = 3;
+    const userVisited = pageNumber * userPerPage;
+    const pageCount = Math.ceil(users.length / userPerPage)
+
+    const displayUsers = users.slice(userVisited, userVisited + userPerPage).map(user => <User
+        key={user.id}
+        user={user}
+    ></User>)
 
 
     useEffect(() => {
@@ -11,16 +22,27 @@ const Home = () => {
             .then(data => setUsers(data))
     }, [])
 
+    const changePage = ({ selected }) => {
+        setPageNumber(selected)
+    }
+
     return (
         <div className='w-3/4  mx-auto mt-10'>
             <h1 className='text-5xl font-bold text-black mb-10'>Users Information</h1>
             <div className='border border-solid border-black rounded-xl shadow-xl'>
-                {
-                    users && users.map(user => <User
-                        key={user.id}
-                        user={user}
-                    ></User>)
-                }
+                {displayUsers}
+
+                <ReactPaginate
+                    previousLabel={"<"}
+                    nextLabel={">"}
+                    pageCount={pageCount}
+                    onPageChange={changePage}
+                    containerClassName={"pegination"}
+                    previousLinkClassName={"previousButton"}
+                    nextLinkClassName={"nextButton"}
+                    disabledClassName={"disableButton"}
+                    activeClassName={"activeButton"}
+                ></ReactPaginate>
             </div>
         </div>
     );
--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,68 +1,74 @@
 import React, { useState } from 'react';
 
-const User = ({ user }) => {
+
+const User = ({ user, pageCount, setPageNumber }) => {
     const [show, setShow] = useState(false);
+
+
     return (
-        <div className="card mx-5 mt-10 mb-16 border border-solid bg-base-100 shadow-xl">
-            <div className="card-body flex-row justify-between items-center">
+        <div>
+            <div className="card mx-5 mt-10 mb-10 border border-solid bg-base-100 shadow-xl">
+                <div className="card-body flex-row justify-between items-center">
 
-                <div>
-                    <h2 className='font-bold text-xl'>Company</h2>
-                    <h3>{user.company.name}</h3>
-                </div>
+                    <div>
+                        <h2 className='font-bold text-xl'>Company</h2>
+                        <h3>{user.company.name}</h3>
+                    </div>
 
-                <div>
-                    <h2 className='font-bold text-xl'>Contact</h2>
-                    <h3>{user.name}</h3>
-                </div>
-                <div>
-                    <h2 className='font-bold text-xl'>City</h2>
-                    <h3>{user.address.city}</h3>
-                </div>
-                <div>
-                    <h2 className='font-bold text-xl'>Street</h2>
-                    <h3>{user.address.street}</h3>
-                </div>
-                <div>
-                    {
-                        show ? <button onClick={() => setShow(false)} className="btn btn-info rounded-3xl text-white">Hide Details</button>
-                            : <button onClick={() => setShow(true)} className="btn btn-info rounded-3xl text-white">View Details</button>
-                    }
-                </div>
-
-            </div>
-            {
-                show && <div className='grid md:grid-cols-1 lg:grid-cols-2 gap-4  border border-solid rounded-2xl m-10 p-10'>
-                    <div className='text-start'>
-                        <h2 className='font-bold '>Contact Person</h2>
+                    <div>
+                        <h2 className='font-bold text-xl'>Contact</h2>
                         <h3>{user.name}</h3>
                     </div>
-                    <div className='text-start'>
-                        <h2 className='font-bold '>City</h2>
+                    <div>
+                        <h2 className='font-bold text-xl'>City</h2>
                         <h3>{user.address.city}</h3>
                     </div>
-                    <div className='text-start'>
-                        <h2 className='font-bold '>Street</h2>
+                    <div>
+                        <h2 className='font-bold text-xl'>Street</h2>
                         <h3>{user.address.street}</h3>
                     </div>
-                    <div className='text-start'>
-                        <h2 className='font-bold '>Username</h2>
-                        <h3>{user.username}</h3>
+                    <div>
+                        {
+                            show ? <button onClick={() => setShow(false)} className="btn btn-info rounded-3xl text-white">Hide Details</button>
+                                : <button onClick={() => setShow(true)} className="btn btn-info rounded-3xl text-white">View Details</button>
+                        }
                     </div>
-                    <div className='text-start'>
-                        <h2 className='font-bold '>E-mail</h2>
-                        <h3>{user.email}</h3>
-                    </div>
-                    <div className='text-start'>
-                        <h2 className='font-bold '>Phone</h2>
-                        <h3>{user.phone}</h3>
-                    </div>
-                    <div className='text-start'>
-                        <h2 className='font-bold '>Website</h2>
-                        <h3>{user.website}</h3>
-                    </div>
+
                 </div>
-            }
+                {
+                    show && <div className='grid md:grid-cols-1 lg:grid-cols-2 gap-4  border border-solid rounded-2xl m-10 p-10'>
+                        <div className='text-start'>
+                            <h2 className='font-bold '>Contact Person</h2>
+                            <h3>{user.name}</h3>
+                        </div>
+                        <div className='text-start'>
+                            <h2 className='font-bold '>City</h2>
+                            <h3>{user.address.city}</h3>
+                        </div>
+                        <div className='text-start'>
+                            <h2 className='font-bold '>Street</h2>
+                            <h3>{user.address.street}</h3>
+                        </div>
+                        <div className='text-start'>
+                            <h2 className='font-bold '>Username</h2>
+                            <h3>{user.username}</h3>
+                        </div>
+                        <div className='text-start'>
+                            <h2 className='font-bold '>E-mail</h2>
+                            <h3>{user.email}</h3>
+                        </div>
+                        <div className='text-start'>
+                            <h2 className='font-bold '>Phone</h2>
+                            <h3>{user.phone}</h3>
+                        </div>
+                        <div className='text-start'>
+                            <h2 className='font-bold '>Website</h2>
+                            <h3>{user.website}</h3>
+                        </div>
+                    </div>
+                }
+            </div>
+
         </div>
     );
 };
